675a103dea4c333d2b67405863cefc779a4e0202
Killua <elatarmohamed158@gmail.com>
2024-02-26 01:46:16 +0200

    Start point

---
+++ app/src/main/java/com/graduation/breastcancer/ui/SplashScreenActivity.kt
@@ -0,0 +1,33 @@
+package com.graduation.breastcancer.ui
+
+import android.content.Intent
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import com.graduation.breastcancer.databinding.ActivityMainBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        initViews()
+    }
+
+    private fun initViews() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        startSplash()
+    }
+
+    private fun startSplash() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, RegestraionCycleActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
