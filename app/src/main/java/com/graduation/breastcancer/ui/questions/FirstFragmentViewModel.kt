5f11f6bc92e368454964aad52c6c0f85c0de64c5
Killua <elatarmohamed158@gmail.com>
2024-02-27 16:03:07 +0200

    Questionnaire first oage validation

--- app/src/main/java/com/graduation/breastcancer/ui/questions/FirstFragmentViewModel.kt
+++ app/src/main/java/com/graduation/breastcancer/ui/questions/FirstFragmentViewModel.kt
@@ -4,15 +4,19 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class FirstFragmentViewModel : ViewModel() {
-    val maritalStatus: String? = null
+    var maritalStatus: String? = null
+    var gender: String? = null
     val errorName = MutableLiveData<String>()
     val errorAge = MutableLiveData<String>()
+    val maritalStatusError = MutableLiveData<String>()
+    val genderError = MutableLiveData<String>()
     val errorFirstAddress = MutableLiveData<String>()
     val errorRegionAddress = MutableLiveData<String>()
     val Name = MutableLiveData<String>()
     val Age = MutableLiveData<String>()
     val FirstAddress = MutableLiveData<String>()
     val RegionAddress = MutableLiveData<String>()
+
     fun insertData() {
         if (!isValid()) {
             return
@@ -47,14 +51,29 @@ class FirstFragmentViewModel : ViewModel() {
         } else {
             errorRegionAddress.postValue("")
         }
+        if (maritalStatus.isNullOrBlank()) {
+            valid = false
+            maritalStatusError.postValue("Required")
+        } else {
+            maritalStatusError.postValue("")
+        }
+        if (gender.isNullOrBlank()) {
+            valid = false
+            genderError.postValue("Required")
+        } else {
+            genderError.postValue("")
+        }
+
+
         return valid
     }
 
-    fun getGender() {
-
+    fun getGender(gender: String) {
+        this.gender = gender
     }
 
-    fun getMaritalStatus() {
+    fun getMaritalStatus(maritalStatus: String) {
+        this.maritalStatus = maritalStatus
 
     }
 
