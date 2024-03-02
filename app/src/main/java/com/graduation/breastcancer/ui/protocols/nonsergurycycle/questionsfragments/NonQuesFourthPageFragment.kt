8bd87ae7c7e78ce57d43b2036caa6d1e49acc535
Killua <elatarmohamed158@gmail.com>
2024-03-02 15:27:54 +0200

    first protocol added

--- app/src/main/java/com/graduation/breastcancer/ui/protocols/nonsergurycycle/questionsfragments/NonQuesFourthPageFragment.kt
+++ app/src/main/java/com/graduation/breastcancer/ui/protocols/nonsergurycycle/questionsfragments/NonQuesFourthPageFragment.kt
@@ -5,8 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.graduation.breastcancer.ARG_PARAM1
-import com.graduation.breastcancer.ARG_PARAM2
 import com.graduation.breastcancer.R
 
 /**
@@ -15,17 +13,7 @@ import com.graduation.breastcancer.R
  * create an instance of this fragment.
  */
 class NonQuesFourthPageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,23 +23,4 @@ class NonQuesFourthPageFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_non_ques_fourth_page, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NonQuesFourthPageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NonQuesFourthPageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
